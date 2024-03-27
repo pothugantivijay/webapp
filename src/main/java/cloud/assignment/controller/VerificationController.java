05c69ec4aad076f0eb1d2f7575bc31fed1337f64
pothugantivijay <144852299+pothugantivijay@users.noreply.github.com>
2024-03-27 19:57:25 -0400

    Update VerificationController.java

--- src/main/java/cloud/assignment/controller/VerificationController.java
+++ src/main/java/cloud/assignment/controller/VerificationController.java
@@ -1,41 +1,49 @@
 package cloud.assignment.controller;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import org.slf4j.Logger;
-
-import java.util.Date;
+import cloud.assignment.model.TokenEntity;
+import cloud.assignment.model.User;
 import cloud.assignment.repository.TokenRepository;
+import cloud.assignment.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.Optional;
 
 @RestController
 public class VerificationController {
-        private static final Logger logger = LoggerFactory.getLogger(VerificationController.class);
 
     @Autowired
     private TokenRepository tokenRepository;
 
     @GetMapping("/verify")
-    public String verifyToken(@RequestParam String token) {
+    public ResponseEntity<?> verifyuser(HttpServletRequest req, @RequestParam("token") String tok) {
+
         try {
-            return tokenRepository.findByTokenAndVerifiedIsFalse(token)
-                    .map(tokenEntity -> {
-                        if (tokenEntity.getExpiration().after(new Date())) {
-                            tokenEntity.setVerified(true);
-                            tokenRepository.save(tokenEntity);
-                            return "Verified successfully";
-                        } else {
-                            return "Verification link expired";
-                        }
-                    })
-                    .orElse("Invalid token");
+            Optional<TokenEntity> t = tokenRepository.findByLink(tok);
+            if (t.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            TokenEntity tk = t.get();
+            Timestamp ts = tk.getExptime();
+
+            long exp_time = ts.getTime();
+            Timestamp now = new Timestamp(System.currentTimeMillis());
+            long curr_time = now.getTime();
+
+            if (exp_time - curr_time > 0) {
+
+                tk.setVerified(true);
+                tokenRepository.save(tk);
+                return ResponseEntity.status(HttpStatus.OK).body("User Verified !!");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Verification Request timed out. Try again !!");
+            }
         } catch (Exception e) {
-            logger.error("error hit"+ e.getMessage());
-            //logger.error(e.getStackTrace());
-            return "An error occurred while verifying the token";
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e);
         }
     }
 }
