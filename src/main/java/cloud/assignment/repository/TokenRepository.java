65777c833777765465664128275baa32ed213bb7
pothugantivijay <144852299+pothugantivijay@users.noreply.github.com>
2024-03-27 20:08:42 -0400

    Update TokenRepository.java

--- src/main/java/cloud/assignment/repository/TokenRepository.java
+++ src/main/java/cloud/assignment/repository/TokenRepository.java
@@ -1,8 +1,12 @@
 package cloud.assignment.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-import cloud.assignment.model.*;
 
+import cloud.assignment.model.TokenEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
 public interface TokenRepository extends JpaRepository<TokenEntity, String> {
-    Optional<TokenEntity> findByTokenAndVerifiedIsFalse(String token);
+    Optional<TokenEntity> findByLink(String link);
 }
